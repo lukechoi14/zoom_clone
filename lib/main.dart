292e2d38ffe0df4cc9f8736477f64f24cdbd6379
luke choi <primera_jh@hotmail.com>
2022-03-23 20:36:51 +0900

    home screen using stream builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/resources/auth_methods.dart';
 import 'package:zoom_clone/screens/home_screen.dart';
 import 'package:zoom_clone/screens/login_screen.dart';
 import 'package:zoom_clone/utils/colors.dart';
@@ -23,7 +24,22 @@ class MyApp extends StatelessWidget {
         '/login' : (context) => const LoginScreen(),
         '/home' : (context) => const HomeScreen()
       },
-      home: LoginScreen()
+      home: StreamBuilder(
+        stream: AuthMethods().authChanges,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+
+          return const LoginScreen();
+        },
+      ),
     );
   }
 }
