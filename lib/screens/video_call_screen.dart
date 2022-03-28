e9131a27f2408fcc3dd4969eb37309dc073e55d1
luke choi <primera_jh@hotmail.com>
2022-03-28 23:49:37 +0900

    video call screen

---
+++ lib/screens/video_call_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class VideoCallScreen extends StatefulWidget {
+  const VideoCallScreen({Key? key}) : super(key: key);
+
+  @override
+  _VideoCallScreenState createState() => _VideoCallScreenState();
+}
+
+class _VideoCallScreenState extends State<VideoCallScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
