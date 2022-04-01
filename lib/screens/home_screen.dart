3ba2df9c8402a7bbc20f12b66511e63c566208aa
luke choi <primera_jh@hotmail.com>
2022-04-01 22:38:39 +0900

    home screen UI done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../resources/auth_methods.dart';
+import '../utils/colors.dart';
+import 'history_meeting_screen.dart';
+import 'meeting_screen.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
@@ -15,8 +20,58 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  List<Widget> pages = [
+    MeetingScreen(),
+    const HistoryMeetingScreen(),
+    const Text('Contacts'),
+    CustomButton(text: 'Log Out', onPressed: () => AuthMethods().signOut()),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+        title: const Text('Meet & Chat'),
+        centerTitle: true,
+      ),
+      body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: footerColor,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        onTap: onPageChanged,
+        currentIndex: _page,
+        type: BottomNavigationBarType.fixed,
+        unselectedFontSize: 14,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.comment_bank,
+            ),
+            label: 'Meet & Char',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.lock_clock,
+            ),
+            label: 'Meetings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+            ),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings_outlined,
+            ),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
   }
 }
