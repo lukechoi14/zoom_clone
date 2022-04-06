1baa61aa423b151bc83879042f3758de3b4f41cf
luke choi <primera_jh@hotmail.com>
2022-04-06 22:27:26 +0900

    flutter clean

--- lib/screens/video_call_screen.dart
+++ lib/screens/video_call_screen.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:jitsi_meet/jitsi_meet.dart';
 import 'package:zoom_clone/resources/auth_methods.dart';
 
 import '../resources/jitsi_meet_methods.dart';
 import '../utils/colors.dart';
+import '../widgets/meeting_option.dart';
 
 class VideoCallScreen extends StatefulWidget {
   const VideoCallScreen({Key? key}) : super(key: key);
